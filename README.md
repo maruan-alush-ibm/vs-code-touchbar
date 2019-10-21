13ccd76984a5e03080c4a1ec4a27a5a1383d9948
Maruan Alush <maruan.alush@ibm.com>
2019-10-21 14:21:24 +0100

    Major changes: Updated read.ME; Added two new buttons - Test / Start; Changed how commands are sent to VS code from npm-script.run to focusing on the terminal and sending text to it.

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# README
+# VS Code TouchBar
 
-This is my WIP vs-code-touchbar extension
+VS Code TouchBar is a WIP project that I am developing to allow for customisable buttons on a MacBook Touch Bar.
+
+## Installation
+
+You can download the VS code extension [online](https://marketplace.visualstudio.com/items?itemName=Maruan-Alush.vs-code-touchbar) or on the native VS Code marketplace.
+
+## Usage
+
+You can edit your settings in VS code to change what each button does or for those more tech savvy you can edit your settings.json.
+
+```json
+"touchbar.test": "test:unit",
+"touchbar.build": "build:local",
+"touchbar.run": "run:offline",
+"touchbar.start": "start:dev"
+```
+The ```touchbar.test``` is the name of the button for which your are changing the command and the ```test:unit``` is the new command that you want it to run.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,51 @@
 import * as vscode from 'vscode';
 
+const terminal = vscode.window.terminals[0];
+
+function sendTextFunc(setting: string){
+
+	let temp = vscode.workspace.getConfiguration('touchbar').get(setting);
+
+	vscode.commands.executeCommand('workbench.action.terminal.focus');
+	terminal.show();
+	vscode.commands.executeCommand('workbench.action.terminal.clear');
+	
+	terminal.sendText("npm run " + String(temp));
+
+}
+
+
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "vs-code-touchbar" is now active!');
 
 	let NPM_RUN = vscode.commands.registerCommand('extension.NPM_run', () => {
 
-		let x = vscode.workspace.getConfiguration('touchbar').get('run');
-
-		vscode.commands.executeCommand('npm-script.' + String(x));
+		let temp = vscode.workspace.getConfiguration('touchbar').get("run");
+		
+		vscode.commands.executeCommand('npm-script.run');
 	});
 
 	let NPM_BUILD = vscode.commands.registerCommand('extension.NPM_build', () => {
 
-		let x = vscode.workspace.getConfiguration('touchbar').get('build');
-		
-		vscode.commands.executeCommand('npm-script.' + String(x));
+		sendTextFunc("build");
 
 	});
 
-	context.subscriptions.push(NPM_RUN, NPM_BUILD);
+	let NPM_START = vscode.commands.registerCommand('extension.NPM_start', () => {
+
+		sendTextFunc("start");
+
+
+	});
+
+	let NPM_TEST = vscode.commands.registerCommand('extension.NPM_test', () => {
+
+		sendTextFunc("test");
+
+	});
+
+	context.subscriptions.push(NPM_RUN, NPM_BUILD, NPM_START, NPM_TEST);
 }
 
 
